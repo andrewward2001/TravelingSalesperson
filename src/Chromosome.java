c03490ca9090513be0225e54d51b57d1525f765a
dima618 <dimailinemail@gmail.com>
2017-12-04 08:23:54 -0500

    calcDist

--- src/Chromosome.java
+++ src/Chromosome.java
@@ -1,14 +1,39 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 /**
  * Created by student on 12/2/17.
  */
 public class Chromosome {
     private ArrayList<City> cities = new ArrayList<City>();
+    private double dist;
+    private int sequences = 100;
 
-    public Chromosome(){
+    public Chromosome(ArrayList<City> cities){
+        this.cities = cities;
+
 
     }
+
+    public void mutate(){
+
+
+    }
+    public double calcDist(){
+        if(dist!=0){
+            return dist;
+        }
+        for (int i = 0; i < cities.size(); i++) {
+            dist += cities.get(i).distanceTo(cities.get(i+1));
+        }
+
+        return dist;
+    }
+
+//    public Chromosome[] mate(){
+//
+//
+//    }
 
 
 }
