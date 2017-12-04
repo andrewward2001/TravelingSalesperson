870e2b73cf7037ffc24dcb845539646efffc20d5
dima618 <dimailinemail@gmail.com>
2017-12-04 07:48:27 -0500

    friggin classes and stuff

---
+++ src/Chromosome.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+/**
+ * Created by student on 12/2/17.
+ */
+public class Chromosome {
+    private ArrayList<City> cities = new ArrayList<City>();
+
+    public Chromosome(){
+
+    }
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * Created by student on 12/2/17.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+
+
+}
