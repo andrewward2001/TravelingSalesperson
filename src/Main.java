78dbaff55375aba9df2cc1e2a613773252d636a9
Andrew Ward <andrew_ward@student.wayland.k12.ma.us>
2017-12-04 07:48:09 -0500

    fake it till you make it

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
 
 public class Main {
-    public static void main(String[] args) {
 
+
+
+    public static void main(String[] args) {
+        TestCities cities = new TestCities();
+        City[] cities1 = cities.getCities();
+        for(int i = 0; i < cities1.length-1; i++) {
+            System.out.println(cities1[i].distanceTo(cities1[i+1]));
+        }
     }
+
 }
